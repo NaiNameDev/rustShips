bb51dd9c8253eec1d37b13dcfdb329f5794f5ce9
NaiNameDev <nonameguy1000@gmail.com>
2024-10-05 23:43:40 +0300

    bad_polygon

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,126 @@
 use rand::Rng;
 use std::thread;
+use round::round_down;
 
 mod tilemap;
 
-static mut gravity_x: i16 = 1;
-static mut gravity_y: i16 = 1;
+const AIR_SYM: char = '.';
+
+static mut GRAVITY_X: i16 = 1;
+static mut GRAVITY_Y: i16 = 1;
 
 //default 250
-static delta: u32 = 25;
-pub static mut phys: Vec<tilemap::Object> = vec![];
+static DELTA: u32 = 50;
+pub static mut objects: Vec<tilemap::Object> = vec![];
+pub static mut polygon_objects: Vec<tilemap::PolygonObject> = vec![];
 
 pub fn update(){
-    let mut tmp_tile_map: tilemap::TileMap = tilemap::TileMap::new('~');
+    let mut tmp_tile_map: tilemap::TileMap = tilemap::TileMap::new(AIR_SYM, 32, 32);
     unsafe{
-        for i in 0..phys.len(){
-            tmp_tile_map.set_sym(phys[i].position, phys[i].sym);
+
+        // POLYGONS
+        for i in 0..polygon_objects.len() {
+            for j in 0..polygon_objects[i].points.len() {
+                tmp_tile_map.set_sym(polygon_objects[i].points[j], polygon_objects[i].sym);
+            }
+
+            let mut float_plus: f64 = 0.0;
+            for j in 0..polygon_objects[i].points.len() {
+                println!("its itr: {}", j);
+                let mut tmp1: tilemap::Vec2 = tilemap::Vec2::def();
+                let mut tmp2: tilemap::Vec2 = tilemap::Vec2::def();
+
+                if j != polygon_objects[i].points.len() - 1 {
+                    tmp1 = polygon_objects[i].points[j];
+                    tmp2 = polygon_objects[i].points[j + 1];
+                }
+                else {
+                    tmp1 = polygon_objects[i].points[polygon_objects[i].points.len() - 1];
+                    tmp2 = polygon_objects[i].points[0];
+                }
+
+
+                let vector: tilemap::Vec2 = tilemap::Vec2::new(tmp1.x - tmp2.x, tmp1.y - tmp2.y);
+                vector.print();
+                let mut oper_x: i16 = 0;
+                let mut oper_y: i16 = 0;
+
+                if vector.x >= 0 { oper_x = -1; }
+                else { oper_x = 1 }
+
+                if vector.y >= 0 { oper_y = -1; }
+                else { oper_y = 1}
+
+                let mut dir: f64 = vector.y as f64 / vector.x as f64;
+                if dir < 0.0 { dir *= -1.0; }
+
+                println!("{}", dir);
+                
+                let mut tmp: tilemap::Vec2 = tilemap::Vec2::def();
+                for o in 0..64 {
+                    float_plus += dir - round_down(dir, 0);
+                    let mut clear_dir: i16 = round_down(dir, 0) as i16;
+                    
+                    let mut now_pos: tilemap::Vec2 = tilemap::Vec2::def();
+                    
+                    for l in 0..clear_dir + round_down(float_plus, 0) as i16 {
+                        now_pos = tilemap::Vec2::new(polygon_objects[i].points[j].x + (tmp.x * oper_x), polygon_objects[i].points[j].y + (tmp.y * oper_y));
+                        tmp_tile_map.set_sym(now_pos, polygon_objects[i].sym);
+                        tmp.y += 1;
+                    }
+                    tmp.x += 1;
+
+                        now_pos = tilemap::Vec2::new(polygon_objects[i].points[j].x + (tmp.x * oper_x), polygon_objects[i].points[j].y + (tmp.y * oper_y));
+                        tmp.y += 1;
+
+                    /*if vector.x == 0 {
+                        let mut opr: i16 = 1;
+                        if vector.y < 0 { opr *= -1; }
+
+                        for l in 0..vector.y * opr as i16 {
+                            now_pos = tilemap::Vec2::new(polygon_objects[i].points[j].x + (tmp.x * oper_x), polygon_objects[i].points[j].y + (l * oper_y));
+                            tmp_tile_map.set_sym(now_pos, polygon_objects[i].sym);
+                        }
+                        break;
+                    }
+                    for l in 0..clear_dir + round_down(float_plus, 0) as i16 {
+                        now_pos = tilemap::Vec2::new(polygon_objects[i].points[j].x + (tmp.x * oper_x), polygon_objects[i].points[j].y + (tmp.y * oper_y));
+                        tmp_tile_map.set_sym(now_pos, polygon_objects[i].sym);
+                        tmp.y += 1;
+                    }
+                    tmp.x += 1;
+
+                    //if (clear_dir + round_down(float_plus, 0) as i16) < 1 {
+                        now_pos = tilemap::Vec2::new(polygon_objects[i].points[j].x + (tmp.x * oper_x), polygon_objects[i].points[j].y + (tmp.y * oper_y));
+                        tmp_tile_map.set_sym(now_pos, polygon_objects[i].sym);
+                    //}*/
+                    if now_pos == tmp2 { break; }
+
+                    if float_plus >= 1.0 {
+                        float_plus -= 1.0;
+                    }
+                }
+            }
+        }
+        
+        //SINGLE PIXELS
+        for i in 0..objects.len(){
+            tmp_tile_map.set_sym(objects[i].position, objects[i].sym);
         }
 
-        for i in 0..phys.len(){
-            let mut tmp = &mut phys[i];
+        for i in 0..objects.len(){
+            let tmp = &mut objects[i];
 
             if tmp.is_static == false {
-                if tmp_tile_map.get_sym(tilemap::Vec2::new(tmp.position.x, tmp.position.y + gravity_y)) == '~'{
-                    tmp_tile_map.set_sym(phys[i].position, '~');
-                    tmp.moveto(tilemap::Vec2::new(tmp.position.x, tmp.position.y + gravity_y));
-                    tmp_tile_map.set_sym(phys[i].position, phys[i].sym);
+                if tmp_tile_map.get_sym(tilemap::Vec2::new(tmp.position.x, tmp.position.y + GRAVITY_Y)) == AIR_SYM {
+                    tmp_tile_map.set_sym(objects[i].position, AIR_SYM);
+                    tmp.moveto(tilemap::Vec2::new(tmp.position.x, tmp.position.y + GRAVITY_Y));
+                    tmp_tile_map.set_sym(objects[i].position, objects[i].sym);
                 }
-                if tmp_tile_map.get_sym(tilemap::Vec2::new(tmp.position.x + gravity_x, tmp.position.y)) == '~'{
-                    tmp_tile_map.set_sym(phys[i].position, '~');
-                    tmp.moveto(tilemap::Vec2::new(tmp.position.x + gravity_x, tmp.position.y));
-                    tmp_tile_map.set_sym(phys[i].position, phys[i].sym);
+                if tmp_tile_map.get_sym(tilemap::Vec2::new(tmp.position.x + GRAVITY_X, tmp.position.y)) == AIR_SYM {
+                    tmp_tile_map.set_sym(objects[i].position, AIR_SYM);
+                    tmp.moveto(tilemap::Vec2::new(tmp.position.x + GRAVITY_X, tmp.position.y));
+                    tmp_tile_map.set_sym(objects[i].position, objects[i].sym);
                 }
             }
         }
@@ -42,22 +133,45 @@ fn main(){
     let mut frame_count: i32 = 0;
 
     let mut rng = rand::thread_rng();
-    unsafe {
+
+    /*unsafe {
+        let mut trg = tilemap::PolygonObject::new('#', true);
+        trg.add(tilemap::Vec2::new(7, 2));
+        trg.add(tilemap::Vec2::new(2, 2));
+
+        polygon_objects.push(trg);
+    }*/
+
+    /*unsafe {
         let mut added_count: i16 = 0;
         loop {
             let tmp = tilemap::Object::new(tilemap::Vec2::new(rng.gen_range(0..32), rng.gen_range(0..32)), '#', false);
             let mut count: i16 = 0;
 
-            for j in 0..phys.len(){
-                if tmp.position != phys[j].position{ count += 1; }
+            for j in 0..objects.len(){
+                if tmp.position != objects[j].position{ count += 1; }
             }
-            if count as usize == phys.len(){
-                phys.push(tmp);
+            if count as usize == objects.len(){
+                objects.push(tmp);
                 added_count += 1;
                 if added_count == 300 { break; }
             }
         }
-    }
+        let mut added_count: i16 = 0;
+        loop {
+            let tmp = tilemap::Object::new(tilemap::Vec2::new(rng.gen_range(0..32), rng.gen_range(0..32)), 'K', true);
+            let mut count: i16 = 0;
+
+            for j in 0..objects.len(){
+                if tmp.position != objects[j].position{ count += 1; }
+            }
+            if count as usize == objects.len(){
+                objects.push(tmp);
+                added_count += 1;
+                if added_count == 32 { break; }
+            }
+        }
+    }*/
     loop {
         frame_count += 1;
         //for i in 0..36{ println!("\n"); }
@@ -65,10 +179,33 @@ fn main(){
         update();
         
         println!("now frame: {}", frame_count);
-        thread::sleep_ms(delta);
-        
-        unsafe{
-            if frame_count % 41 == 0 { gravity_x *= -1; gravity_y *= -1}
+        thread::sleep_ms(DELTA);
+        unsafe {
+            if frame_count % 1 == 0 {
+                let tmp1 = tilemap::Object::new(tilemap::Vec2::new(rng.gen_range(0..32), rng.gen_range(0..32)), '1', true);
+                let tmp2 = tilemap::Object::new(tilemap::Vec2::new(rng.gen_range(0..32), rng.gen_range(0..32)), '2', true);
+                let tmp3 = tilemap::Object::new(tilemap::Vec2::new(rng.gen_range(0..32), rng.gen_range(0..32)), '3', true);
+                objects.push(tmp1);
+                objects.push(tmp2);
+                objects.push(tmp3);
+                update();
+                println!("now frame: {}", frame_count);
+                thread::sleep_ms(1500);
+
+                let mut trg = tilemap::PolygonObject::new('#', true);
+                trg.add(tilemap::Vec2::new(tmp1.position.x, tmp1.position.y));
+                trg.add(tilemap::Vec2::new(tmp2.position.x, tmp2.position.y));
+                trg.add(tilemap::Vec2::new(tmp3.position.x, tmp3.position.y));
+
+                polygon_objects.push(trg);
+                update();
+                println!("now frame: {}", frame_count);
+                thread::sleep_ms(1500);
+                polygon_objects.remove(0);
+                objects.remove(objects.len() - 1);
+                objects.remove(objects.len() - 1);
+                objects.remove(objects.len() - 1);
+            }
         }
     }
 }
--- src/tilemap.rs
+++ src/tilemap.rs
@@ -1,5 +1,4 @@
 #![allow(incomplete_features)]
-#![feature(generic_const_exprs)]
 
 #[derive(Copy, Clone, PartialEq)]
 pub struct Vec2 {pub x: i16, pub y: i16}
@@ -9,7 +8,7 @@ impl Vec2 {
     }
     pub fn def() -> Vec2{ return Vec2{x: 0, y: 0}; }
     pub fn print(&self){
-        println!("{},{}",self.x, self.y);
+        println!("({},{})",self.x, self.y);
     }
 }
 
@@ -20,12 +19,11 @@ pub struct Object {
     pub is_static: bool,
     pub is_on_floor: bool,
 }
-
 impl Object {
     pub fn new(pos: Vec2, sym: char, stat: bool/*, toadd: &mut Vec<Object>*/) -> Object {
-        let mut newObj = Object{position: pos, sym: sym, is_static: stat, is_on_floor: false};
+        let new_obj = Object{position: pos, sym: sym, is_static: stat, is_on_floor: false};
         //toadd.push(self);
-        return newObj;
+        return new_obj;
     }
     pub fn moveto(&mut self, new_pos: Vec2) {
         self.position = new_pos;
@@ -35,24 +33,44 @@ impl Object {
     }
 }
 
-const GLOBAL_X: usize = 32;
-const GLOBAL_Y: usize = 32;
+pub struct PolygonObject {
+    pub points: Vec<Vec2>,
+    pub sym: char,
+    pub is_static: bool
+}
+impl PolygonObject {
+    pub fn new(sym: char, stat: bool) -> PolygonObject {
+        let new_obj = PolygonObject{ points: vec![] , sym: sym, is_static: stat };
+        return new_obj;
+    }
+    pub fn add(&mut self, pos: Vec2) {
+        self.points.push(pos);
+    }
+    pub fn move_point(&mut self, index: i16, new_pos: Vec2) {
+        self.points[index as usize] = new_pos;
+    }
+    pub fn rename(&mut self, new_sym: char) {
+        self.sym = new_sym;
+    }
+}
 
 pub struct TileMap {
-    arr: [(char, Vec2); GLOBAL_X * GLOBAL_Y],
+    arr: Vec<(char, Vec2)>,
+    size: Vec2
 }
 impl TileMap{
-    pub fn new(sym: char) -> TileMap {
-        let mut tmp_world: TileMap = TileMap{ arr: [(sym,Vec2::def()); (GLOBAL_X * GLOBAL_Y) as usize]};
+    pub fn new(sym: char, x: i16, y: i16) -> TileMap {
+        //let mut tmp_world: TileMap = TileMap{ arr: [(sym,Vec2::def()); (x * y) as usize]};
+        let mut tmp_world: TileMap = TileMap { arr: vec![(sym,Vec2::def()); (x * y) as usize], size: Vec2::new(x, y)};
         let mut tmp_pos = Vec2::new(0,0);
 
-        for i in 0..GLOBAL_X * GLOBAL_Y{
-            if tmp_pos.x < (GLOBAL_X - 1) as i16{
-                tmp_world.arr[i].1 = Vec2::new(tmp_pos.x, tmp_pos.y);
+        for i in 0..x * y{
+            if tmp_pos.x < (x - 1) as i16{
+                tmp_world.arr[i as usize].1 = Vec2::new(tmp_pos.x, tmp_pos.y);
                 tmp_pos.x += 1;
             }
             else{
-                tmp_world.arr[i].1 = Vec2::new(tmp_pos.x, tmp_pos.y);
+                tmp_world.arr[i as usize].1 = Vec2::new(tmp_pos.x, tmp_pos.y);
 
                 tmp_pos.y += 1;
                 tmp_pos.x = 0;
@@ -65,19 +83,19 @@ impl TileMap{
         let mut tmp_pos: Vec2 = Vec2::new(0,0);
         let mut tmp_string: String = "".to_string();
         
-        for i in 0..GLOBAL_X * GLOBAL_Y{
-            if tmp_pos.x < (GLOBAL_X - 1) as i16{
+        for i in 0..self.size.x * self.size.y{
+            if tmp_pos.x < (self.size.x - 1) as i16{
                 tmp_pos.x += 1;
                 
                 tmp_string += " ";
-                tmp_string += &(self.arr[i].0).to_string();
+                tmp_string += &(self.arr[i as usize].0).to_string();
             }
             else{
                 tmp_pos.y += 1;
                 tmp_pos.x = 0;
                 
                 tmp_string += " ";
-                tmp_string += &(self.arr[i].0).to_string();
+                tmp_string += &(self.arr[i as usize].0).to_string();
                 tmp_string += "\n";
             }
         }
